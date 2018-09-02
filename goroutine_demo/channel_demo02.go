68a2c7726b8a12ac04ea77e5f8a10348565dad5d
caoyuewen <caoyuewen@qq.com>
2018-09-02 18:30:33 +0800

    goroutine demo02

--- goroutine_demo/channel_demo02.go
+++ goroutine_demo/channel_demo02.go
@@ -5,3 +5,41 @@
 */
 package main
 
+import (
+	"time"
+	"fmt"
+)
+
+var mapChan = make(chan map[string]int,1)
+
+func main() {
+	syncChan := make(chan struct{}, 2)
+
+	go func() {
+		for {
+			if elem, ok := <-mapChan; ok {
+				elem["count"]++
+			} else {
+				break
+			}
+		}
+		syncChan <- struct{}{}
+	}()
+
+	go func() {
+		countMap := make(map[string]int)
+		for i := 0; i < 5; i++ {
+			//contMap["count"] = i
+			mapChan <- countMap
+			time.Sleep(time.Microsecond)
+			fmt.Printf("The count map:%v.[sender]\n", countMap)
+		}
+		fmt.Println("Stoped. [receiver]")
+		close(mapChan)
+		syncChan <- struct{}{}
+	}()
+
+	<-syncChan
+	<-syncChan
+
+}
